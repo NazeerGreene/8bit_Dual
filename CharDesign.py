f8ed8fad1614882d85872239f5be02309e47d110
NazeerGreene <58005647+NazeerGreene@users.noreply.github.com>
2019-12-16 23:30:12 -0800

    Zones
    - object zones
    - randomZone to pick a random zone for the object
    - new advance/update method for objects that move them to their zones and adjusts

--- CharDesign.py
+++ CharDesign.py
@@ -1,69 +1,91 @@
 from time import time
 from pygwidgets import *
+import random
 
 Y_MIN = 350
-Y_MAX = 50
+
 X_MIN = 20
 X_MAX = 750
-MOVE = 4
 
 class Character:
 
     def __init__(self):
         pass
 
-
     def draw(self):
         self.image.draw()
+
+    def setZones(self, zoneList):
+        """Sets the zone attribute for the object;
+           enter a nested list of [zone, Zone_starting_location]"""
+        
+        if zoneDict == []:
+            return -1
+        self.zones = zoneList
+
+    
+    def randomZone(self):
+        """Gets a random zone from the user defined list, so that the object may randomly
+            place itself."""
+        
+        if self.zones == []:
+            return -1
+        
+        zone, zoneStart = random.choice(zones)
+        self.advance(zoneStart)
+        return zone
         
 
 class Enemy(Character):
 
-
     def __init__(self, window, x):
-        self.health = 20
-        self.max = Enemy.x_max
+        #self.health = 5
         self.picture = "pictures/8bit_enemy.png"
         self.x = x
         self.y = Y_MIN
+        self.zones = []
         self.window = window
         self.image = Image(self.window, (self.x, self.y), self.picture)
-        Enemy.x_max -= 100
         
 
-    def advance(self):
-        self.x += 50
-        if self.x > self.max:
-            self.x = self.max
-            
+    def advance(self, x, adjust = 60):
+        """To move the Enemy object to the new location;
+            adjust is used to fit the entire picture into the zone."""
+        
+        self.x = x - adjust
+        if self.x < X_MIN:
+            self.x = X_MIN
+
         self.image.setLoc((self.x, self.y))
+
         
 
 class player(Character):
 
 
     def __init__(self, window, x):
-        #self.username = username
         self.health = 50
         self.picture = "pictures/8bit_player.png"
         self.x = x
         self.y = Y_MIN
+        self.__zones = []
         self.window = window
         self.image = Image(self.window, (self.x, self.y), self.picture)
 
-    def update(self, leftOrRight):
-        if leftOrRight == 'right':
-            self.x += MOVE
-            if self.x > X_MAX:
-                self.x = X_MAX
-            #print("X POSITION", self.x_cord)
-        else:
-            self.x -= MOVE
-            if self.x < X_MIN:
-                self.x = X_MIN
-            #print("X POSITION", self.x_cord)
-                
+    def advance(self, x, adjust = 20):
+        """To move the Player object to the new location;
+            adjust is used to fit the entire picture into the zone."""
+        
+        self.x = x + adjust
+        if self.x > X_MAX:
+            self.x = X_MAX
+                            
         self.image.setLoc((self.x, self.y))
+
+
+    def attacked(self):
+        self.health -= 5
+
 
 
 class Bullet(Character):
@@ -76,11 +98,9 @@ class Bullet(Character):
         self.startLoc = (self.x, self.y)
         self.image = Image(self.window, self.startLoc, self.picture)
 
-    def update(self):
-        
-        self.x -= 10
+    def update(self, x):
         self.image.setLoc((self.x,self.y))
         
     @property
     def offScreen(self):
-        return self.x < X_MIN
+        return self.x < X_MIN or self.x > X_MAX
